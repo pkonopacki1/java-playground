8111e0ae79ea1c0501d709e7d791c3d168edd70a
pkonopacki1 <konopackipio@gmail.com>
2021-04-27 14:46:52 +0200

    Added few lines.

--- Primitive types/src/App.java
+++ Primitive types/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // TODO: Add your firs lesson
+
+        // Below won't compile, bigger type can't be conver to lower
+        // byte bVar = 128;
+        
+        // The other way round is possible
+        byte bVar = 127;
+        int iVar = bVar;
+
+        // Float and double differnce
+        // float ends with ...f
+        float fVar = 1.3f;
+        double dVar = 2.3;
+
+        // Same as with integers, won't comiple
+        // fVar =  dVar;
+
+        // This works fine
+        dVar = fVar;
+
+
     }
+
 }
