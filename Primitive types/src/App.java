2a6b8c37de8489956790c764d4069e4ef74dd494
pkonopacki1 <konopackipio@gmail.com>
2021-05-05 15:45:24 +0200

    Added few lines

--- Primitive types/src/App.java
+++ Primitive types/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Below won't compile, bigger type can't be conver to lower
+        // Below won't compile, bigger type can't be convert to lower
         // byte bVar = 128;
         
         // The other way round is possible
@@ -13,11 +13,21 @@ public class App {
         float fVar = 1.3f;
         double dVar = 2.3;
 
-        // Same as with integers, won't comiple
+        // Same as with integers, won't compile
         // fVar =  dVar;
 
         // This works fine
         dVar = fVar;
+
+        // This also won't work, byte+byte is converted to int automatically
+        byte x = 5;
+        byte y = 10;
+        // byte z = x + y;
+
+        //----- Other possible integers notations -----//
+        int a = 0b0011;
+        int b = 1_000_000;
+        System.out.println(a);        
 
 
     }
