d34020bff814e01a619a945146c78ef064012619
pkonopacki1 <konopackipio@gmail.com>
2021-05-06 11:06:53 +0200

    Completed primitives, added new project

---
+++ TextNumberDate/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //-----Strings----//
+        // Important thing about strings is that they are immutable objects. So all string opeartions, create new string
+        
+
+
+    }
+}
