347c4ddf640c854a3e2474aa83dae67fac862848
pkonopacki1 <konopackipio@gmail.com>
2021-05-13 15:17:24 +0200

    Reformated Variables

---
+++ java-variables/src/test/java/com/pkonopacki1/Integers.java
@@ -0,0 +1,27 @@
+package com.pkonopacki1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class Integers 
+{
+    @Test
+    // We can do it using casting but if value exceeds size, overflow will happen
+    public void valueWillOverflowWhenDowncasting()
+    {
+        byte bVarCast = (byte) 128;
+        assertEquals(-128, bVarCast);
+    }
+
+    @Test(expected = java.lang.Error.class)
+    // Larger value than type size is not possible because of checked exception
+    public void exceptionWillBeThrownWhenExceedingVariableSize() {
+        byte bVar = 128;
+    }
+
+    @Test
+    public void smallToLargeIsPossible()
+}
