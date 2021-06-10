8c4aa4115758334e6c6c63df909a594028a91ab4
Piotr Konopacki <konopackipio@gmail.com>
2021-06-10 18:13:15 +0100

    Added test cases for Strings

---
+++ text-number-date/src/main/java/com/pkonopacki1/Strings.java
@@ -0,0 +1,9 @@
+package com.pkonopacki1;
+
+public class Strings {
+    public static void main(String[] args) {
+        String testString = "test";
+        System.out.println(testString);
+    }
+    
+}
--- text-number-date/src/test/java/com/pkonopacki1/StringsTest.java
+++ text-number-date/src/test/java/com/pkonopacki1/StringsTest.java
@@ -1,6 +1,9 @@
 package com.pkonopacki1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -15,6 +18,24 @@ public class StringsTest
         String test = "test";
         String test1 = "test";
 
-        assertEquals(test.hashCode(), test1.hashCode());
+        assertTrue(test == test1);
+    }
+
+    // New String() creates new object. We can use .intern() to point to same place
+    @Test
+    public void stringNewConstructorCreatesNewObect() {
+        String test = "test";
+        String test1 = new String("test");
+
+        assertTrue(test != test1);
+        assertTrue(test == test1.intern());
+    }
+
+    // Remember that when using substring, first index is inclusive, the last one is not
+    @Test
+    public void sungstringIncludesFirstIndexNotLast() {
+        String test = "abcdedfg";
+
+        assertEquals("bcd", test.substring(1, 4));
     }
 }
