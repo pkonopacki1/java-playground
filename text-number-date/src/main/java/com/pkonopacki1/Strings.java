e61714442491813ec8e20257870cb1fc7baf80f4
Piotr Konopacki <konopackipio@gmail.com>
2021-06-13 09:24:01 +0100

    Added String methods

--- text-number-date/src/main/java/com/pkonopacki1/Strings.java
+++ text-number-date/src/main/java/com/pkonopacki1/Strings.java
@@ -2,8 +2,27 @@ package com.pkonopacki1;
 
 public class Strings {
     public static void main(String[] args) {
-        String testString = "test";
-        System.out.println(testString);
+        // Some useful String methods
+        String testString = "This is a test string";
+
+        //Length
+        System.out.println(testString.length());
+
+        //equalsIgnoreCase
+        String newTeString = testString.toUpperCase();
+        System.out.println(newTeString.equalsIgnoreCase(testString));
+
+        //CompareTo, metoda  porównuje na podstawie kolejności alfabetycznej
+        //Zwrócona <0 wartość oznacza że obiekt który WYWOŁUJE metodę jest pierwszy w kolejności
+        //Duże litery są pierwsze
+        System.out.println(newTeString.compareTo(testString));
+
+        //trim
+        String notTrimmedString = "   String to be trimmed   ";
+        System.out.println(notTrimmedString);
+        System.out.println(notTrimmedString.trim());
+
+        System.out.println("cat".compareTo("dog"));
     }
     
 }
