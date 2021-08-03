fb0f9a4d062ffc72623cedf55f0878ce20f9e64f
pkonopacki1 <konopackipio@gmail.com>
2021-08-03 11:04:35 +0200

    Added casting test class

---
+++ oop/src/main/java/com/pkonopacki1/CastingClass.java
@@ -0,0 +1,19 @@
+package com.pkonopacki1;
+
+import java.util.ArrayList;
+
+public class CastingClass {
+    public static void main(String[] args) {
+        ArrayList<Number> numberList = new ArrayList<>();  
+        ArrayList<Integer> integerList = new ArrayList<>();
+        
+        // Below code shows that Arrays need to be of exact type
+        // numberList = integerList;
+        // integerList = numberList;
+    }
+    
+    
+
+  
+    
+}
