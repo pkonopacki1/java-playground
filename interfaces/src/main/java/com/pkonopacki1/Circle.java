404aa459d204aa62b549abecc8e1d6b1793cb4b7
Piotr Konopacki <konopackipio@gmail.com>
2021-08-04 18:04:39 +0100

    Added new classes

---
+++ interfaces/src/main/java/com/pkonopacki1/Circle.java
@@ -0,0 +1,11 @@
+package com.pkonopacki1;
+
+public class Circle extends Figure{
+
+    public Circle(Point figurePosition) {
+        super(figurePosition);
+    }
+
+    
+    
+}
--- interfaces/src/main/java/com/pkonopacki1/Interfaces.java
+++ interfaces/src/main/java/com/pkonopacki1/Interfaces.java
@@ -5,8 +5,17 @@ public class Interfaces
     public static void main( String[] args )
     {
         MovableSquare square = new MovableSquare(new Point(0.0, 0.0));
-        System.out.println(square.getFigurePostion());
-        square.move(5.0, 10.0);
-        System.out.println(square.getFigurePostion());        
+        MovableCircle circle = new MovableCircle(new Point(10.0, 10.0));
+
+        Movable[] movableObjects = new Movable[]{square, circle};
+
+        for (Movable movable : movableObjects) {
+            Figure movableFigure = (Figure) movable;
+            System.out.println(movableFigure.getClass() + " start postion: " + movableFigure.getFigurePostion());
+            movable.move(5.0, 10.0);
+            System.out.println(movableFigure.getClass() + " end postion: " + movableFigure.getFigurePostion());
+        }
+        
+       
     }
 }
