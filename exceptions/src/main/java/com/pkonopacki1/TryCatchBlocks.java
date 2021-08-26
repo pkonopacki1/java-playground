8816fa2d73ca8be4b2be9e912f60e81326e9c026
pkonopacki1 <konopackipio@gmail.com>
2021-08-26 11:34:40 +0200

    Added method showing try-finally block Added property file

--- exceptions/src/main/java/com/pkonopacki1/TryCatchBlocks.java
+++ exceptions/src/main/java/com/pkonopacki1/TryCatchBlocks.java
@@ -1,7 +1,9 @@
 package com.pkonopacki1;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 // Class showing different possibilities of try-catch-fianlly blocks
 
@@ -9,7 +11,7 @@ public class TryCatchBlocks {
 
     public static void main(String[] args) {
         try {
-            FileInputStream tFileInputStream = new FileInputStream("IDontExtist.txt");
+            String property = readPropertyFromFile("propertyfile.txt");
         } catch(FileNotFoundException f) {
             System.out.println("File not found exception block: ");
             Exceptions.printParentClasses(f);
@@ -17,6 +19,24 @@ public class TryCatchBlocks {
             System.out.println("Throwable block: ");
             Exceptions.printParentClasses(t);
         }
+    }
+
+
+    // Below functions show how try-finally block looks like. Finally is executed ALWAYS
+    private static String readPropertyFromFile(String fileName) throws IOException {
+        String property;
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        try {
+            String line = br.readLine();
+            property = line.split("\\s")[1];
+            System.out.println("Property: " + property);
+        } finally {
+            System.out.println("Finally block execution");
+            if(br != null) br.close();
+        }
+        return property;
+
     }
     
 }
