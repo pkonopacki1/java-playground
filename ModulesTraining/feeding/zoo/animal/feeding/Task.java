7ecaabdcc59d8a9921c9c25815534def77b8e0ab
pkonopacki1 <konopackipio@gmail.com>
2021-09-07 22:37:08 +0200

    Update for module project

--- ModulesTraining/feeding/zoo/animal/feeding/Task.java
+++ ModulesTraining/feeding/zoo/animal/feeding/Task.java
@@ -2,6 +2,7 @@ package zoo.animal.feeding;
 
 public class Task {
     public static void main(String[] args) {
-        System.out.println("I'm feed");
+        System.out.println("I'm being fed!");
     }
+    
 }
