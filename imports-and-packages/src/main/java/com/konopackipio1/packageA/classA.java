98cd9a4e324a1f070206f4c57c3bd4a9c963403a
pkonopacki1 <konopackipio@gmail.com>
2021-09-15 09:27:18 +0200

    Added instructions for compiling and running file form command line

--- imports-and-packages/src/main/java/com/konopackipio1/packageA/classA.java
+++ imports-and-packages/src/main/java/com/konopackipio1/packageA/classA.java
@@ -1,5 +1,19 @@
 package com.konopackipio1.packageA;
 
+/*
+Test java commands:
+To compile:
+- go to cd src/main/java
+- javac com/konopackipio1/packageA/classA.java com/konopackipio1/packageB/classB.java -d target/classes
+
+To run:
+- go to cd target/classes
+- java com.konopackipio1.packageB.classB
+
+To save as a jar:
+- 
+*/
+
 public class classA {
     public static void methodA() {
         System.out.println("I'm in method A");
