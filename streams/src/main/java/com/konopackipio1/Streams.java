65600e5503c0a4786f7b824c4e532b60a7e2cfdb
pkonopacki1 <konopackipio@gmail.com>
2021-10-15 06:41:18 +0200

    Stream example class added

---
+++ streams/src/main/java/com/konopackipio1/Streams.java
@@ -0,0 +1,41 @@
+package com.konopackipio1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Streams {
+    public static void main(String[] args) {
+        // Creating streams
+        Stream s1 = Stream.empty();
+        Stream s2 = Stream.of(1,2,3);
+
+        Stream s3 = Stream.generate(Math::random);
+        // s3.limit(10).forEach(System.out::println);
+
+        List list = new ArrayList<>(List.of(1,2,3));
+        Stream s4 = list.stream();
+
+
+        // Using Stream.Builder
+        Stream.Builder<String> sBuilder = Stream.<String>builder();
+        String test = "one two three";
+        for(String s: test.split("\\s")) {
+            sBuilder.accept(s);            
+        }
+        Stream s5 = sBuilder.build();
+        s5.forEach(System.out::println);
+
+
+        // Using terminal operation twice
+        s2.forEach(System.out::println);
+        try {
+            s2.count();
+        } catch (IllegalStateException e) {
+            System.out.println("You can't use terminal operation on a stream that already has been prcossed: " + e);
+        }
+        
+
+    }
+    
+}
