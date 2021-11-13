587d16e99d577f61c963eea738ac7353ad594aee
pkonopacki1 <konopackipio@gmail.com>
2021-11-13 10:44:51 +0100

    New examples of nested classes added

---
+++ oop/src/main/java/com/pkonopacki1/nested/LocalClassExample.java
@@ -0,0 +1,30 @@
+package com.pkonopacki1.nested;
+
+
+class OuterLocalClass {
+    private int length = 10;
+    private int height = 5;
+    
+    public OuterLocalClass() {
+        height = height *2;
+    }
+
+    public void outerClassMethod() {
+        int width;
+        width = 5;
+        // width = 10; // If this line would be ucommented, widht would no longer be effectively final
+        // and could no longer be uses in a local class
+        class LocalClass {
+            private int volume() {
+                // height is not effectively final but it is an instance field, not a local one
+                return length * width * height;
+            }
+
+            
+        }
+    }
+}
+
+public class LocalClassExample {
+    
+}
