bdc8f92fe8a62133c40cb4553856722844fadf84
pkonopacki1 <konopackipio@gmail.com>
2021-11-29 18:52:14 +0100

    Some small changes

--- ModulesTraining/feeding/module-info.java
+++ ModulesTraining/feeding/module-info.java
@@ -1,18 +1,18 @@
 /* 
 -------------To compile module--------------
-javac --module-path feeding -d bin feeding/zoo/animal/feeding/*.java feeding/module-info.java
---moudle-path specifies where module resides
+javac --module-path mods -d feeding feeding/zoo/animal/feeding/*.java feeding/module-info.java
+--moudle-path specifies where module resides, can be replaced with -p
 -d specifies where files should be put
 at the and we list the files that should be compiled (including module-info.java file)
 
 ------------To run compiled module---------
-java --module-path bin --module zoo.animal.feeding/zoo.animal.feeding.Task
+java --module-path feeding --module zoo.animal.feeding/zoo.animal.feeding.Task
 
 ------------To create jar file-------------
-jar -cvf mods/zoo.animal.feeding.jar -C bin/ .
+jar -cvf mods/zoo.animal.feeding.jar -C feeding/ .
 
 ------------To run jar file
-java -p mods --module zoo.animal.feeding/zoo.animal.feeding.Task
+java -p mods -m zoo.animal.feeding/zoo.animal.feeding.Task
 
 */
 
