62a526161f23ecec79c486d8b0855b4c266ad365
pkonopacki1 <konopackipio@gmail.com>
2022-05-11 13:07:10 +0200

    Changed arrays examples class

--- arrays-and-collections/src/main/java/com/pkonopacki1/ArraysManipulation.java
+++ arrays-and-collections/src/main/java/com/pkonopacki1/ArraysManipulation.java
@@ -1,28 +1,35 @@
 package com.pkonopacki1;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.ListIterator;
+import java.util.Random;
 
+/**
+ * This class show usefullness of Arrays class
+ */
 public class ArraysManipulation {
     public static void main(String[] args) {
-        Integer[] intArray = new Integer[10];
+        // Inside this method you can see first use of Arrays class
+        int[] testArr = createRandomArray(10, 100);
+        System.out.println("Array before manipulation: " + Arrays.toString(testArr));
 
-        Arrays.setAll(intArray, (x) -> x*x);
-        Arrays.sort(intArray, Collections.reverseOrder());
+        int searchedInt = testArr[4];
 
-        System.out.println(Arrays.toString(intArray));
+        // Sorting
+        Arrays.sort(testArr);
+        System.out.println("Array after sorting: " + Arrays.toString(testArr));
 
-        // Using of iterator
-        ListIterator it = Arrays.asList(intArray).listIterator();
+        System.out.println("Inedx of " + searchedInt + ": " + Arrays.binarySearch(testArr, searchedInt));
+        // In case of not found returns -index-1
+        System.out.println("Inedx for number not found " + 101 + ": " + Arrays.binarySearch(testArr, 101));
+    }
 
-        while(it.hasNext()) {
-            System.out.print(it.next() + " ");
-        }
-        System.out.println("\n----Reverese order----");
-        while(it.hasPrevious()) {
-            System.out.print(it.previous() + " ");
-        }
+    private static int[] createRandomArray(int size, int max) {
+        Random r = new Random();
+        int[] arr = new int[size];
+
+        Arrays.setAll(arr, i -> i = r.nextInt(max));
+
+        return arr;
     }
     
 }
